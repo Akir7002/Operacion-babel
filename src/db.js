888a834dfff5762d8b231624a0e705db7656cc7a
Maria Fernanda Patiño <mariafernandap941@gmail.com>
2026-09-21 20:37:53 -0500

    Rama Maria Fernanda Patino Carranza: frontend + diseno (Pages, Css, Js, Mockups, Wireframes)

--- src/db.js
+++
@@ -1,4 +0,0 @@
-// Adaptador de la capa de datos: reutiliza el pool de PostgreSQL de db.js (raiz).
-const { query, getClient, getPool } = require('../db');
-
-module.exports = { query, getClient, getPool };
